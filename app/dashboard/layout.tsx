f5c7401fab4cd33822574e58ff9f7012efd3701b
IonasNeonakis <ionas.neonakis@gmail.com>
2025-03-22 23:22:29 +0100

    chapter 4: Creating Layouts and Pages

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import {ReactNode} from "react";
+
+interface DashBoardLayoutProps {
+    children: ReactNode
+}
+
+
+export default function Layout({children}: DashBoardLayoutProps) {
+    return (
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav/>
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+    );
+}
